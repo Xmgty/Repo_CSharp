d2080929aed630cda320a99ae9eda51e548bbe77
Xmgty <36451940+Xmgty@users.noreply.github.com>
2018-10-30 16:22:43 +0300

    Add files via upload

---
+++ Store/Store/Source/Option.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    [Serializable]
+    class Option
+    {
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return String.Format("{0}", Name);
+        }
+
+    }
+}
