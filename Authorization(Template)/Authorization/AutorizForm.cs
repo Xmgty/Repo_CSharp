4a2d70c0c9d410a6429b59141bd1cd7aa737e890
Xmgty <36451940+Xmgty@users.noreply.github.com>
2019-01-15 21:07:18 +0300

    Add files via upload

---
+++ Authorization(Template)/Authorization/AutorizForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Authorization
+{
+    public partial class AutorizForm : Form
+    {
+        
+
+        public AutorizForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void AutorizForm_Load(object sender, EventArgs e)
+        {
+
+
+        }
+     
+
+        private void linkPass_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            PasswordForm frmPass = new PasswordForm();
+            frmPass.ShowDialog();
+        }
+
+        private void linkReg_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            RegistrForm frmReg = new RegistrForm();
+            frmReg.ShowDialog();
+        }
+
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
